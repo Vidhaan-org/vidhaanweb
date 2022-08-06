e222ac753d1a57cd79a01668b1e328f799ec218c
dewanshDT <dewansh.dt@gmail.com>
2022-08-06 15:51:59 +0530

    our cases and more components

--- pages/dashboard/cases.tsx
+++ pages/dashboard/cases.tsx
@@ -1,7 +1,113 @@
 import React from "react"
+import { BsPin, BsPinAngleFill, BsSearch } from "react-icons/bs"
+import Button from "../../src/components/Button"
+import Select from "../../src/components/Select"
 
 const Cases = () => {
-  return <div>Cases</div>
+  return (
+    <div className="flex px-10 w-full h-full flex-col">
+      <div className="flex mt-14 mx-4 mb-16 h-full flex-col">
+        <div className="flex gap-4">
+          <div className="flex bg-gray-200 rounded-md text-gray-500 items-center gap-3 px-3 py-2 text-lg border-gray-300 border-2">
+            <BsSearch />
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="search"
+              className="bg-transparent outline-none min-w-[30vw]"
+            />
+          </div>
+          {/* <Button type="fill" color="accent-orange">
+          <div className="items-center flex gap-2">
+            <BsSearch />
+            <div className="">Search</div>
+          </div>
+        </Button> */}
+        </div>
+        <div className="grid gap-7 grid-cols-2 mt-5">
+          <div className="flex  gap-4 flex-col">
+            <div className="text-xl font-semibold text-accent-blue">
+              Filter By
+            </div>
+            <div className="grid grid-cols-3 gap-2">
+              <div className="flex flex-col gap-1">
+                <div className="text-gray-600 text-sm font-semibold">
+                  Case Status
+                </div>
+                <Select options={["lksdjf", "lskdjfl", "lsdkfjl"]} />
+              </div>
+              <div className="flex flex-col gap-1">
+                <div className="text-gray-600 text-sm font-semibold">Court</div>
+                <Select options={["lksdjf", "lskdjfl", "lsdkfjl"]} />
+              </div>
+              <div className="flex flex-col gap-1">
+                <div className="text-gray-600 text-sm font-semibold">Year</div>
+                <Select options={["lksdjf", "lskdjfl", "lsdkfjl"]} />
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4 flex-col">
+            <div className="text-xl font-semibold text-accent-blue">
+              Sort By
+            </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="flex flex-col gap-1">
+                <div className="text-gray-600 text-sm font-semibold">Date</div>
+                <Select options={["lksdjf", "lskdjfl", "lsdkfjl"]} />
+              </div>
+              <div className="flex flex-col gap-1">
+                <div className="text-gray-600 text-sm font-semibold">Name</div>
+                <Select options={["lksdjf", "lskdjfl", "lsdkfjl"]} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex mt-10 flex-col w-full bg-gray-100 rounded-md border border-gray-300">
+          <div className="grid grid-cols-5 font-semibold p-2 bg-gray-300 text-accent-blue">
+            <div className="uppercase">cnr</div>
+            <div className="">Case Name</div>
+            <div className="">Date</div>
+            <div className="">Case Status</div>
+          </div>
+          <TableRow />
+          <TableRow />
+          <TableRow />
+          <TableRow />
+          <TableRow />
+          <TableRow />
+          <TableRow />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const TableRow = () => {
+  return (
+    <div className="grid grid-cols-5 p-2 items-center hover:bg-gray-200">
+      <div className="mr-5 line-clamp-1">CGHC01-043756-2019</div>
+      <div className="mr-5 line-clamp-1">
+        P.Suseela vs University Grants commission
+      </div>
+      <div className="mr-5 line-clamp-1">13 July 2019</div>
+      <div className="flex">
+        <div className="rounded-full cursor-default text-white font-semibold px-3 py-1 mr-auto bg-orange-300">
+          Ongoing
+        </div>
+      </div>
+      <div className="grid grid-cols-3 items-center gap-3">
+        <Button
+          type="fill"
+          color="accent-blue"
+          className="col-span-2 opacity-60"
+        >
+          <div className="w-full text-center">View Details</div>
+        </Button>
+        <BsPinAngleFill className="text-xl ml-auto mr-8 hover:text-red-400 text-gray-500" />
+      </div>
+    </div>
+  )
 }
 
 export default Cases
--- pages/dashboard/home.tsx
+++ pages/dashboard/home.tsx
@@ -1,10 +1,38 @@
 import React from "react"
+import {
+  BsBell,
+  BsBellFill,
+  BsGear,
+  BsGearFill,
+  BsInfoCircle,
+} from "react-icons/bs"
 import Button from "../../src/components/Button"
 
 const Home = () => {
   return (
     <div className="flex h-full flex-col w-full px-8">
-      <div className="flex mt-8">ss000</div>
+      <div className="flex w-full gap-6 mt-8">
+        <div className="flex w-full items-center bg-gray-100 rounded shadow-md overflow-hidden">
+          <div className="px-4 text-white gap-3 flex font-semibold py-3 items-center bg-accent-orange">
+            <BsInfoCircle />
+            <div className="whitespace-nowrap">Urgent action Required</div>
+          </div>
+          <div className="flex gap-3 px-4 w-full">
+            <div className="rounded-full text-white font-semibold px-3 py-1 bg-orange-300">
+              12 June 2022
+            </div>
+            <div className="rounded-full text-white font-semibold px-3 py-1 bg-blue-300">
+              12 June 2022
+            </div>
+            <div className="flex items-center ml-4 font-semibold text-gray-800">
+              Lorem ipsum dolor sit amet.
+            </div>
+          </div>
+        </div>
+        <Button type="fill" color="accent-orange">
+          <div className="px-6">Go</div>
+        </Button>
+      </div>
       <div className="grid text-2xl gap-5 py-6 grid-cols-3">
         <Button
           type="fill"
@@ -12,7 +40,7 @@ const Home = () => {
           className="justify-center py-9 opacity-60"
           color="accent-blue"
         >
-          Cases
+          <div className="py-6">Our Cases</div>
         </Button>
         <Button
           type="fill"
@@ -20,7 +48,7 @@ const Home = () => {
           className="justify-center py-9 opacity-60"
           color="accent-blue"
         >
-          Cases
+          <div className="py-6">FilePetition</div>
         </Button>
         <Button
           type="fill"
@@ -28,7 +56,7 @@ const Home = () => {
           className="justify-center py-9 opacity-60"
           color="accent-blue"
         >
-          Cases
+          <div className="py-6">Hearing Schedule</div>
         </Button>
       </div>
       <div className="w-full mb-8 h-full grid gap-5 grid-cols-4">
@@ -37,18 +65,24 @@ const Home = () => {
         <Button
           type="fill"
           width="auto"
-          className="justify-center py-9 opacity-60"
+          className="justify-center opacity-60"
           color="accent-blue"
         >
-          Cases
+          <div className="flex flex-col items-center gap-4 text-xl">
+            <BsGearFill size={60} />
+            <div className="">Settings</div>
+          </div>
         </Button>
         <Button
           type="fill"
           width="auto"
-          className="justify-center py-9 opacity-60"
+          className="justify-center opacity-60"
           color="accent-blue"
         >
-          Cases
+          <div className="flex flex-col items-center gap-4 text-xl">
+            <BsBellFill size={60} />
+            <div className="">Notifications &amp; Deadlines</div>
+          </div>
         </Button>
       </div>
     </div>
